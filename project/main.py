d426d85c7cdb194b59588bc68d8af21b15863e83
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2022-04-24 16:37:20 +0700

    Add files via upload

--- project/main.py
+++ project/main.py
@@ -6,6 +6,16 @@ from flask_login import current_user, login_required
 main = Blueprint('main', __name__)
 
 
+@main.route('/loader')
+def loader():
+    return render_template('loader.html'), {"Refresh": "3; url=http://127.0.0.1:5000/conteiner"}
+
+
+@main.route('/conteiner')
+def conteiner():
+    return render_template('index.html')
+
+
 @main.route('/')
 def index():
     return render_template('index.html')
@@ -16,6 +26,3 @@ def index():
 def profile():
     name = current_user.name
     return render_template('profile.html', name=name)
-
-
-
